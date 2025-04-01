cb41e214ae1b5c37e5c88869c3a59c08fd563730
Joshua Loo <looshua@Joshuas-MacBook-Pro-5.local>
2025-04-01 18:52:24 -0400

    Added the low rank minimizer code in.

--- L3RM/utils.py
+++ L3RM/utils.py
@@ -10,3 +10,7 @@ def get_matrix_gradient(matrix: np.ndarray,
     dely = dy @ matrix
 
     return delx, dely
+
+def soft_shrinkage(matrix: np.ndarray,
+                   tau: float):
+    return np.sign(matrix)*np.max(np.abs(matrix)-tau,0)
