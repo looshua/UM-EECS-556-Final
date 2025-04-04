4ef1a35856e7a5246b6d5634c16771d4cb9bb1f3
Joshua Loo <looshua@Joshuas-MacBook-Pro-5.local>
2025-04-03 22:08:27 -0400

    Updated utils.py

--- L3RM/utils.py
+++ L3RM/utils.py
@@ -1,7 +1,15 @@
 import numpy as np
+from scipy import sparse
 
-def first_order_derivative_filter(n,k=1):
-    return np.eye(n, k=k)-np.eye(n)
+def first_order_derivative_filter(n,k=1, is_sparse=False):
+    if is_sparse:
+        return sparse.eye_array(n, k=k)-sparse.eye_array(n)
+    else:
+        return np.eye(n, k=k)-np.eye(n)
+
+def sparse_place_mat_on_diag(matrix: np.ndarray):
+    mat_flat = matrix.ravel()
+    return sparse.diags_array(mat_flat)
 
 def get_matrix_gradient(matrix: np.ndarray, 
                         dx: np.ndarray, 
